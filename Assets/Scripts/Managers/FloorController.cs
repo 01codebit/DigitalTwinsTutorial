1481aa4494fcb47f64b50ae95268230c47f8ac3b
l.serra <l.serra@white3.it>
2026-03-16 16:57:50 +0100

    challenge: floor dashboard

--- Assets/Scripts/Managers/FloorController.cs
+++ Assets/Scripts/Managers/FloorController.cs
@@ -2,18 +2,34 @@ using System;
 using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class FloorController : MonoBehaviour
 {
-    [SerializeField] private GameObject _exteriorBuilding;
+    [SerializeField]
+    private GameObject _exteriorBuilding;
+
+    [SerializeField]
+    private UIDocument _dashboard;
+
+    private VisualElement _dashboardRoot;
+    private VisualElement _dashboardElement;
+
+    private float _leftDelta = 0f;
 
     private CinemachineCamera _exteriorCamera;
     private CinemachineCamera _topDownCamera;
 
-
     private void Awake()
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
+
+        _dashboardRoot = _dashboard.rootVisualElement;
+        _dashboardElement = _dashboardRoot.Q<VisualElement>("Dashboard");
+        _leftDelta = 620f;
+        _dashboardElement.style.left = -1 * _leftDelta;
+
+        Debug.Log($"[FloorController] Left delta set to {_leftDelta}");
     }
 
     private void OnDestroy()
@@ -25,12 +41,16 @@ public class FloorController : MonoBehaviour
     {
         if (_exteriorCamera == null)
         {
-            _exteriorCamera = GameObject.FindWithTag("ExteriorCam")?.GetComponent<CinemachineCamera>();
+            _exteriorCamera = GameObject
+                .FindWithTag("ExteriorCam")
+                ?.GetComponent<CinemachineCamera>();
         }
 
         if (_topDownCamera == null)
         {
-            _topDownCamera = GameObject.FindWithTag("TopDownCam")?.GetComponent<CinemachineCamera>();
+            _topDownCamera = GameObject
+                .FindWithTag("TopDownCam")
+                ?.GetComponent<CinemachineCamera>();
         }
 
         if (_exteriorBuilding == null)
@@ -40,7 +60,9 @@ public class FloorController : MonoBehaviour
                 if (rootObject.CompareTag("ExteriorBuilding"))
                 {
                     _exteriorBuilding = rootObject;
-                    Debug.Log($"[FloorController] Found exterior building in scene '{scene.name}'.");
+                    Debug.Log(
+                        $"[FloorController] Found exterior building in scene '{scene.name}'."
+                    );
                     break;
                 }
             }
@@ -61,10 +83,20 @@ public class FloorController : MonoBehaviour
 
         _topDownCamera.Priority = 20;
         _exteriorCamera.Priority = 10;
+
+        ActivateDashboard(floorId);
+    }
+
+    private void ActivateDashboard(string floorId)
+    {
+        _dashboardElement.style.left = 20f;
+        _dashboardRoot.Q<Label>("Title").text = TwinRegistry.Instance.Floors[floorId].DisplayName;
     }
 
     public void ActivateExterior()
     {
+        _dashboardElement.style.left = -1 * _leftDelta;
+
         if (_exteriorBuilding)
         {
             _exteriorBuilding.SetActive(true);
